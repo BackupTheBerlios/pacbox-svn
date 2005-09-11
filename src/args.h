6d8e39b42bb6fdd2aa429b37db1cc94efd6020d9
highlander <highlander@e4abd8ac-8bff-0310-83fe-bdd6e02ca443>
2005-09-11 12:16:24 +0000

    * Renamned the files and structs (better to be short)

---
+++ src/args.h
@@ -0,0 +1,25 @@
+#ifndef _ARGUMENTS_H_
+#define _ARGUMENTS_H_
+
+#include <stdlib.h>
+#include "linked_list.h"
+
+/* Struct containg all the 
+ * arguments from the command-line */
+typedef struct {
+	int			verbose;		/* Sets verbose output */
+	int			ask;			/* The program should ask first */
+	int			pretend;		/* The program just shows the packages */
+	LinkedList	*package_list;	/* A list of packages */
+} Args;
+
+/* Creates a new args struct */ 
+Args * args_new ();
+
+/* Sets artuct to default values */
+static void args_init (Args *args);
+
+/* Destroys the struct */
+void args_destroy (Args *args);
+
+#endif 
