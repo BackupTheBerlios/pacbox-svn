a89941a649c0c20c5cbb3cbaa1a238fedcedf888
highlander <highlander@e4abd8ac-8bff-0310-83fe-bdd6e02ca443>
2006-01-23 13:14:40 +0000

    * Removed old ocde * Fixed some misc changes for the package

--- trunk/src/pacbox.c
+++ trunk/src/pacbox.c
@@ -1,101 +1,10 @@
-#include "package.h"
-#include "string_utils.h"
-#include "linked_list.h"
-#include "global_config.h"
-#include "args.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-/* Prints the usage for the program */
-void print_usage (const char *name);
-
-/* Prints the helptext */
-void print_help ();
-
-/* Prints info for the compile and so forth */
-void print_info (GlobalConfig *conf);
-
 int main (int argc, char **argv)
 {
-	GlobalConfig conf;
-	Args *args = args_new ();
-	global_config_set_defaults (&conf);
-	
-	/* this ocours when the user
-	 * dont supply arguments */
-	if (argc == 1)
-	{
-		print_help ();
-	}
-	else
-	{	
-		/* Parses all the arguments */
-		args_parse (args, argc, argv);
-
-		/* Prints some info */
-		if (args->verbose)
-		{
-			ListNode *it;
-			printf ("Going to install packages:\n");
-			for (it = args->package_list->first;
-				 it != args->package_list->last->next;
-				 it = it->next)
-			{
-				printf ("%s\n", (char *)it->data);
-			}
-		}
-
-		if (args->ask)
-		{
-			char c;
-			printf ("\nDo you want to proceed? [Y/n] ");
-			scanf ("%c", &c);
-			
-			if (c == 'Y' || c == 'y' || c == '\n')
-			{
-				/* install */
-				printf ("Installing....\n");
-			}
-		}
-		else	
-		{
-			/* installs the packages one by one */
-			
-			
-			/*
-			* package_install("/home/erik/Desktop/pacbox/echo/PACKAGE", &conf);
-			* */
-		}
-	}
-	
-	/* Removes allocated memory */
-	global_config_destroy (&conf);
-	args_destroy (args);
+	printf ("pacbox v0.1\n");
 	return 0;
-}
-
-void print_usage (const char *name)
-{
-	printf ("Usage:\t%s [args] package or\n\t%s [keyword]\n\n", name, name);
-}
-
-void print_help ()
-{
-	printf ("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
-	printf ("Args\n");
-	printf ("\t-h\t Prints this help text\n");
-	printf ("\t-a\t Asks first if you want to install\n");
-	printf ("\t-v\t Prints some more output\n");
-	printf ("\t-f\t Fetches the packages, not installs them\n");
-	printf ("\nKeywords\n");
-	printf ("\tinfo\t Prints pacbox info and other\n");
-}
-
-void print_info (GlobalConfig *conf)
-{
-	printf ("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
-	global_config_print (conf);
 }
 
